0745a312d7ea7ef9f103e6276d0ffbb074538ba4
dth <info@dthlabs.com>
2023-07-22 17:19:23 -0400

    layout

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <View style={{flex: 1, justifyContent: "center"}}>
+        <Text>Search results:</Text>
+      </View>
+      <View style={{flexDirection: "row"}}>
+        <View style={{flex: 1, marginLeft: 10}}>
+          <TextInput placeholder='Enter your question'/>
+        </View>
+        <TouchableOpacity>
+          <View style={{backgroundColor: "gray", padding: 5, marginRight: 10, marginBottom: 20}}>
+            <Text>Send</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
