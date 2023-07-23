d83761d8e2e55221f5b354a7ba76ea009b463051
dth <info@dthlabs.com>
2023-07-22 20:31:33 -0400

    api

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [inputMessage, setInputMessage] = useState("");
+  
+  const handleButtonClick=()=>{
+    fetch("https://api.openai.com/v1/completions", {
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $apikey"
+      },
+
+      // "model": "text-davinci-003",
+      // "prompt": "Say this is a test",
+      // "max_tokens": 7,
+      // "temperature": 0
+
+      body: JSON.stringify({
+        "prompt": inputMessage,
+        "model": "text-davinci-003"
+      })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+    console.log(inputMessage);
+  }
+
+  const handleTextInput=(text)=>{
+    setInputMessage(text);
+    console.log(text);
+  }
+
   return (
     <View style={styles.container}>
       <View style={{flex: 1, justifyContent: "center"}}>
-        <Text>Search results:</Text>
+        <Text>Results:</Text>
       </View>
       <View style={{flexDirection: "row"}}>
         <View style={{flex: 1, marginLeft: 10}}>
-          <TextInput placeholder='Enter your question'/>
+          <TextInput placeholder='Enter your question' onChangeText={handleTextInput} />
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleButtonClick}>
           <View style={{backgroundColor: "gray", padding: 5, marginRight: 10, marginBottom: 20}}>
             <Text>Send</Text>
           </View>
